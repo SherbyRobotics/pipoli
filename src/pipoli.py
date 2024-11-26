446d4bab70f7747b38d58ee45941e216a704baec
Francisco Pascoa <francisco.pascoa@usherbrooke.ca>
2024-11-26 11:28:54 -0500

    added documentation

--- src/pipoli.py
+++ src/pipoli.py
@@ -51,34 +51,63 @@ Policy = A2C | DDPG | PPO | SAC | TD3
 class DimensionalPolicy:
 
     def __init__(self, policy: Policy, context: Context, obs_space: Box, act_space: Box):
+        """Initialize a dimensional policy from a Stable-Baselines3 policy.
+
+        Parameters:
+        -----------
+        policy: A2C | DDPG | PPO | SAC | TD3
+            the trained Stable-Baselines3 policy
+        context: Context
+            the context of this policy
+        obs_space: Box
+            the observation space on which this policy was trained
+        act_space: Box
+            the action space this policy produced in training
+        """
         self.context = context
         self.policy = policy
         self.obs_space = obs_space
         self.act_space = act_space
 
+    def __rshift__(self, context: Context) -> "ScaledPolicy":
+        return self.to_scaled(context)
+
     def _normalize_obs(self, obs: np.ndarray) -> np.ndarray:
+        """Normalizes the observation in [-1, 1]."""
         low = self.obs_space.low
         high = self.obs_space.high
         return -1 + 2 / (high-low) * (obs-low)
 
     def _denormalize_act(self, norm_act: np.ndarray) -> np.ndarray:
+        """Denormalizes the action from [-1, 1] to `act_space`."""
         low = self.act_space.low
         high = self.act_space.high
         return low + (high-low) / 2 * (norm_act+1)
 
     def predict(self, obs: np.ndarray, *args, **kwargs) -> np.ndarray:
+        """Predicts the dimensional action based on a dimensional observation.
+
+        Should work 1:1 with Stable-Baselines3's predict. It does not support
+        recurrent policies.
+        """
         norm_obs = self._normalize_obs(obs)
         norm_act = self.policy.predict(norm_obs, *args, **kwargs)
         act = self._denormalize_act(norm_act)
         return act
 
     def adim_predict(self, obs_s: np.ndarray, *args, **kwargs) -> np.ndarray:
+        """Predicts the adimensional action based on an adimensional observation.
+
+        Should work with Stable-Baselines3's predict arguments. It does not
+        support recurrent policies.
+        """
         obs = self.context.from_adim_obs(obs_s)
         act = self.predict(obs, *args, **kwargs)
         act_s = self.context.to_adim_act(act)
         return act_s
 
     def to_scaled(self, context: Context) -> "ScaledPolicy":
+        """Returns a `ScaledPolicy` with new `context`."""
         scaled_pol = ScaledPolicy(self, context)
         return scaled_pol
 
@@ -86,10 +115,24 @@ class DimensionalPolicy:
 class ScaledPolicy:
 
     def __init__(self, dim_pol: DimensionalPolicy, context: Context):
+        """Initialize a scaled dimensional policy from a dimensional policy.
+
+        Parameters:
+        -----------
+        dim_pol: DimensionalPolicy
+            the base dimensional policy
+        context: Context
+            the context of the scaled policy
+        """
         self.dim_pol = dim_pol
         self.context = context
 
     def predict(self, obs: np.ndarray, *args, **kwargs) -> np.ndarray:
+        """Predicts the dimensional action based on a dimensional observation.
+
+        Should work 1:1 with Stable-Baselines3's predict. It does not support
+        recurrent policies.
+        """
         obs_s = self.context.to_adim_obs(obs)
         act_s = self.dim_pol.adim_predict(obs_s, *args, **kwargs)
         act = self.context.from_adim_act(act_s)
