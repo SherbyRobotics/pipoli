048ba1f78649959a3876f5228e4f1e90e6df63ee
Francisco Pascoa <francisco.pascoa@usherbrooke.ca>
2025-01-22 17:33:08 -0500

    added Dimension and new Context

--- src/pipoli/core.py
+++ src/pipoli/core.py
@@ -1,7 +1,127 @@
 from abc import abstractmethod, ABC
 import numpy as np
 
-from typing import Callable#, override
+from typing import Callable, Union#, override
+
+
+class Dimension:
+    """Represents the units of quantity in terms of their dimensions.
+    
+    Example:
+    --------
+    >>> M = Dimension([1, 0, 0])  # a kilogram (kg)
+    >>> L = Dimension([0, 1, 0])  # a meter (m)
+    >>> T = Dimension([0, 0, 1])  # a second (s)
+    >>> F = M * L / T**2  # a Newton (N)
+    >>> km = 1000 * L  # a kilometer
+    """
+
+    def __init__(self, powers: np.ndarray, factors: np.ndarray | float=1):
+        """Initialize a dimension.
+
+        Parameters:
+        -----------
+        powers: np.ndarray
+            the power of each base dimension
+        """
+        self.powers = np.array(powers)
+    
+    def __str__(self):
+        return f"Dimension({self.powers})"
+    
+    def __repr__(self):
+        return str(self)
+    
+    def __mul__(self, other: Union["Dimension", float, np.ndarray]) -> "Dimension":
+        if isinstance(other, Dimension):
+            return Dimension(self.powers + other.powers)
+        elif isinstance(other, (float, np.ndarray)):
+            return Dimension(self.powers + np.log10(other))
+        else:
+            return NotImplemented
+
+    def __truediv__(self, other: Union["Dimension", float, np.ndarray]) -> "Dimension":
+        if isinstance(other, Dimension):
+            return Dimension(self.powers - other.powers)
+        elif isinstance(other, (float, np.ndarray)):
+            return Dimension(self.powers - np.log10(other))
+        else:
+            return NotImplemented
+    
+    def __pow__(self, power: float | np.ndarray) -> "Dimension":
+        return Dimension(self.powers * power)
+
+
+class NewContext:
+
+    def __init__(self,
+        base_dimensions: list[Dimension],
+        symbols: list[str],
+        dimensions: dict[str, Dimension],
+        values: dict[str, float]
+    ):
+        """Initialize a context.
+
+        Parameters:
+        -----------
+        base_dimensions: list[Dimension]
+            the base dimensions of the units of the system
+        symbols: list[str]
+            the symbols of the quantities of the system
+        dimensions: dict[str, Dimension]
+            the dimensions of each quantity of the system
+        values: dict[str, float]
+            the value of each quantity of the system
+
+        """
+        self.base_dimensions = base_dimensions
+        self.symbols = symbols
+        self.dimensions = dimensions
+        self.values = values
+    
+    def __str__(self):
+        return f"Context({self.base_dimensions}, {self.symbols}, {self.dimensions}, {self.values})"
+
+    def __repr__(self):
+        return str(self)
+
+    def _compute_factor(self, dimension: Dimension, Binv: np.ndarray, values: np.ndarray) -> float:
+        mi = -Binv @ dimension.powers
+        factor = np.prod(values**mi)
+
+        print(dimension, mi, factor)
+        
+        return factor
+    
+    def make_transforms(self, dimensions: list[Dimension], base: list[str]) -> tuple[Callable[[np.ndarray], np.ndarray], Callable[[np.ndarray], np.ndarray]]:
+        if not all(b in self.symbols for b in base):
+            raise ValueError(f"all symbols in `base` ({base}) must be in the context ({self.symbols})")
+        
+        values = np.array([self.values[b] for b in base])
+
+        dims_base = [self.dimensions[b].powers for b in base]
+        B = np.vstack(dims_base).T
+
+        if np.linalg.matrix_rank(B) != len(self.base_dimensions):
+            return ValueError("the base doesn't span the dimension space")
+        
+        Binv = np.linalg.inv(B)
+
+        factors = np.zeros(len(dimensions))
+        for i, dim in enumerate(dimensions):
+
+            if len(dim.powers) != len(self.base_dimensions):
+                raise ValueError(f"the number of component of the dimension should be the same as the number of base dimensions")
+
+            factors[i] = self._compute_factor(dim, Binv, values)
+
+        def to_adim(x: np.ndarray) -> np.ndarray:
+            return x * factors
+
+        def from_adim(x: np.ndarray) -> np.ndarray:
+            return x / factors
+
+        return to_adim, from_adim
 
 
 class Context:
